0400afea61b8b737922c598896a4290646af2d0c
Richard Lake <ralake1985@gmail.com>
2015-02-27 14:41:41 +0000

    refactor promotional rules with iterator in initialize

--- lib/promotional_rules.rb
+++ lib/promotional_rules.rb
@@ -3,13 +3,16 @@ class PromotionalRules
   attr_reader :spend_limit, :percentage, :code, :price
   
   def initialize(options = {})
-    @spend_limit = options.fetch(:spend_limit) { nil }
-    @percentage = options.fetch(:percentage) { nil }
-    @code = options.fetch(:code) { nil }
-    @price = options.fetch(:price) { nil } 
+    options.each { |parameter, value| instance_variable_set("@#{parameter}", value)}
   end
 
   def apply_to(products)
+    multi_buy_discount(products)
+  end
+
+  private
+
+  def multi_buy_discount(products)
     if products.count { |product| product.code == code } >= 2
       products.each { |product| product.price = price }
     end
